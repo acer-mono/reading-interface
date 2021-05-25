63848d640fe670f31e9813a590f67160e628bd06
IvanovaMV <margaritastar92@gmail.com>
2021-05-25 14:57:58 +0700

    without roles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import MainPage from './views/MainPage/MainPage';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
@@ -7,8 +7,7 @@ import LoginForm from './LoginForm/LoginForm';
 import { useAuth } from './auth';
 import { RoomsView } from './views/RoomsView/RoomsView';
 import { UsersView } from './views/UsersView/UsersView';
-import { useDispatch, useSelector } from 'react-redux';
-import { getUserAsync } from './redux/actions/currentUser';
+import { useSelector } from 'react-redux';
 
 function App() {
   return (
@@ -33,11 +32,6 @@ function App() {
 
 export const PrivateRoute = ({ component: Component, ...rest }) => {
   const [logged] = useAuth();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getUserAsync());
-  }, []);
 
   return (
     <Route
@@ -55,12 +49,7 @@ export const PrivateRoute = ({ component: Component, ...rest }) => {
 
 export const PrivateAdminRoute = ({ component: Component, ...rest }) => {
   const [logged] = useAuth();
-  const dispatch = useDispatch();
   const user = useSelector(store => store.currentUser.user);
-
-  useEffect(() => {
-    dispatch(getUserAsync());
-  }, []);
 
   return (
     <Route
